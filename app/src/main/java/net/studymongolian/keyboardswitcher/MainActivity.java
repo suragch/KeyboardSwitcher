31123f3b32082e14ce0bd68bbf902064a7e6ad6c
suragch <studymongolian@gmail.com>
2018-03-26 11:10:55 +0800

    initial commit

---
+++ app/src/main/java/net/studymongolian/keyboardswitcher/MainActivity.java
@@ -0,0 +1,26 @@
+package net.studymongolian.keyboardswitcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showKeyboardSwitcherMenu();
+    }
+
+    private void showKeyboardSwitcherMenu() {
+        InputMethodManager im = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        if (im == null) return;
+        im.showInputMethodPicker();
+    }
+
+    public void onSwitchKeyboardsButtonClick(View view) {
+        showKeyboardSwitcherMenu();
+    }
+}
